90389ac0d4ab65c905f702d4a80e1b9500947d3e
Cedric Schelfhaut <github@eeckhaut.eu>
2017-07-02 10:40:06 +0200

    Module 5 solution

--- module5-solution/js/script - kopie (2).js
+++ module5-solution/js/script - kopie (2).js
@@ -118,7 +118,6 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = "'" + insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName) + "'";
-      console.log("HTML to insert = " + homeHtmlToInsertIntoMainPage);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
